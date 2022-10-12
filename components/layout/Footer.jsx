1dfbbcbf066e128d39447c2db630eed172275716
adibrisan <adi.brisan@yahoo.com>
2022-10-12 14:01:47 +0300

    Finished footer design

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,117 +1,48 @@
 import React from "react";
+import Image from "next/image";
 
 import styles from "../../styles/Footer.module.css";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea temporibus
-      unde labore, dicta culpa laborum dolore cum blanditiis explicabo
-      repellendus obcaecati praesentium corporis dolorum doloribus iusto ullam
-      magnam quaerat ratione? Lorem ipsum dolor sit, amet consectetur
-      adipisicing elit. Ea temporibus unde labore, dicta culpa laborum dolore
-      cum blanditiis explicabo repellendus obcaecati praesentium corporis
-      dolorum doloribus iusto ullam magnam quaerat ratione? Lorem ipsum dolor
-      sit, amet consectetur adipisicing elit. Ea temporibus unde labore, dicta
-      culpa laborum dolore cum blanditiis explicabo repellendus obcaecati
-      praesentium corporis dolorum doloribus iusto ullam magnam quaerat ratione?
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea temporibus
-      unde labore, dicta culpa laborum dolore cum blanditiis explicabo
-      repellendus obcaecati praesentium corporis dolorum doloribus iusto ullam
-      magnam quaerat ratione? Lorem ipsum dolor sit, amet consectetur
-      adipisicing elit. Ea temporibus unde labore, dicta culpa laborum dolore
-      cum blanditiis explicabo repellendus obcaecati praesentium corporis
-      dolorum doloribus iusto ullam magnam quaerat ratione? Lorem ipsum dolor
-      sit, amet consectetur adipisicing elit. Ea temporibus unde labore, dicta
-      culpa laborum dolore cum blanditiis explicabo repellendus obcaecati
-      praesentium corporis dolorum doloribus iusto ullam magnam quaerat ratione?
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea temporibus
-      unde labore, dicta culpa laborum dolore cum blanditiis explicabo
-      repellendus obcaecati praesentium corporis dolorum doloribus iusto ullam
-      magnam quaerat ratione? Lorem ipsum dolor sit, amet consectetur
-      adipisicing elit. Ea temporibus unde labore, dicta culpa laborum dolore
-      cum blanditiis explicabo repellendus obcaecati praesentium corporis
-      dolorum doloribus iusto ullam magnam quaerat ratione? Lorem ipsum dolor
-      sit, amet consectetur adipisicing elit. Ea temporibus unde labore, dicta
-      culpa laborum dolore cum blanditiis explicabo repellendus obcaecati
-      praesentium corporis dolorum doloribus iusto ullam magnam quaerat ratione?
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea temporibus
-      unde labore, dicta culpa laborum dolore cum blanditiis explicabo
-      repellendus obcaecati praesentium corporis dolorum doloribus iusto ullam
-      magnam quaerat ratione? Lorem ipsum dolor sit, amet consectetur
-      adipisicing elit. Ea temporibus unde labore, dicta culpa laborum dolore
-      cum blanditiis explicabo repellendus obcaecati praesentium corporis
-      dolorum doloribus iusto ullam magnam quaerat ratione? Lorem ipsum dolor
-      sit, amet consectetur adipisicing elit. Ea temporibus unde labore, dicta
-      culpa laborum dolore cum blanditiis explicabo repellendus obcaecati
-      praesentium corporis dolorum doloribus iusto ullam magnam quaerat ratione?
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea temporibus
-      unde labore, dicta culpa laborum dolore cum blanditiis explicabo
-      repellendus obcaecati praesentium corporis dolorum doloribus iusto ullam
-      magnam quaerat ratione? Lorem ipsum dolor sit, amet consectetur
-      adipisicing elit. Ea temporibus unde labore, dicta culpa laborum dolore
-      cum blanditiis explicabo repellendus obcaecati praesentium corporis
-      dolorum doloribus iusto ullam magnam quaerat ratione? Lorem ipsum dolor
-      sit, amet consectetur adipisicing elit. Ea temporibus unde labore, dicta
-      culpa laborum dolore cum blanditiis explicabo repellendus obcaecati
-      praesentium corporis dolorum doloribus iusto ullam magnam quaerat ratione?
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea temporibus
-      unde labore, dicta culpa laborum dolore cum blanditiis explicabo
-      repellendus obcaecati praesentium corporis dolorum doloribus iusto ullam
-      magnam quaerat ratione? Lorem ipsum dolor sit, amet consectetur
-      adipisicing elit. Ea temporibus unde labore, dicta culpa laborum dolore
-      cum blanditiis explicabo repellendus obcaecati praesentium corporis
-      dolorum doloribus iusto ullam magnam quaerat ratione? Lorem ipsum dolor
-      sit, amet consectetur adipisicing elit. Ea temporibus unde labore, dicta
-      culpa laborum dolore cum blanditiis explicabo repellendus obcaecati
-      praesentium corporis dolorum doloribus iusto ullam magnam quaerat ratione?
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea temporibus
-      unde labore, dicta culpa laborum dolore cum blanditiis explicabo
-      repellendus obcaecati praesentium corporis dolorum doloribus iusto ullam
-      magnam quaerat ratione? Lorem ipsum dolor sit, amet consectetur
-      adipisicing elit. Ea temporibus unde labore, dicta culpa laborum dolore
-      cum blanditiis explicabo repellendus obcaecati praesentium corporis
-      dolorum doloribus iusto ullam magnam quaerat ratione? Lorem ipsum dolor
-      sit, amet consectetur adipisicing elit. Ea temporibus unde labore, dicta
-      culpa laborum dolore cum blanditiis explicabo repellendus obcaecati
-      praesentium corporis dolorum doloribus iusto ullam magnam quaerat ratione?
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea temporibus
-      unde labore, dicta culpa laborum dolore cum blanditiis explicabo
-      repellendus obcaecati praesentium corporis dolorum doloribus iusto ullam
-      magnam quaerat ratione? Lorem ipsum dolor sit, amet consectetur
-      adipisicing elit. Ea temporibus unde labore, dicta culpa laborum dolore
-      cum blanditiis explicabo repellendus obcaecati praesentium corporis
-      dolorum doloribus iusto ullam magnam quaerat ratione? Lorem ipsum dolor
-      sit, amet consectetur adipisicing elit. Ea temporibus unde labore, dicta
-      culpa laborum dolore cum blanditiis explicabo repellendus obcaecati
-      praesentium corporis dolorum doloribus iusto ullam magnam quaerat ratione?
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea temporibus
-      unde labore, dicta culpa laborum dolore cum blanditiis explicabo
-      repellendus obcaecati praesentium corporis dolorum doloribus iusto ullam
-      magnam quaerat ratione? Lorem ipsum dolor sit, amet consectetur
-      adipisicing elit. Ea temporibus unde labore, dicta culpa laborum dolore
-      cum blanditiis explicabo repellendus obcaecati praesentium corporis
-      dolorum doloribus iusto ullam magnam quaerat ratione? Lorem ipsum dolor
-      sit, amet consectetur adipisicing elit. Ea temporibus unde labore, dicta
-      culpa laborum dolore cum blanditiis explicabo repellendus obcaecati
-      praesentium corporis dolorum doloribus iusto ullam magnam quaerat ratione?
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea temporibus
-      unde labore, dicta culpa laborum dolore cum blanditiis explicabo
-      repellendus obcaecati praesentium corporis dolorum doloribus iusto ullam
-      magnam quaerat ratione? Lorem ipsum dolor sit, amet consectetur
-      adipisicing elit. Ea temporibus unde labore, dicta culpa laborum dolore
-      cum blanditiis explicabo repellendus obcaecati praesentium corporis
-      dolorum doloribus iusto ullam magnam quaerat ratione? Lorem ipsum dolor
-      sit, amet consectetur adipisicing elit. Ea temporibus unde labore, dicta
-      culpa laborum dolore cum blanditiis explicabo repellendus obcaecati
-      praesentium corporis dolorum doloribus iusto ullam magnam quaerat ratione?
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea temporibus
-      unde labore, dicta culpa laborum dolore cum blanditiis explicabo
-      repellendus obcaecati praesentium corporis dolorum doloribus iusto ullam
-      magnam quaerat ratione? Lorem ipsum dolor sit, amet consectetur
-      adipisicing elit. Ea temporibus unde labore, dicta culpa laborum dolore
-      cum blanditiis explicabo repellendus obcaecati praesentium corporis
-      dolorum doloribus iusto ullam magnam quaerat ratione?
+      <div className={styles.footerImg}>
+        <Image
+          src="/img/footer.jpeg"
+          alt="footer img"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <h3 className={styles.footerTitle}>
+        Oh yes, we did the Chowder&apos;s pizza, well baked slice of pizza.
+      </h3>
+      <div className={styles.addressContainer}>
+        <h4>Find our restaurants</h4>
+        <p className={styles.address}>
+          1654 R. Don Road #304. NewYork, 85022 (602) 867-1010
+        </p>
+        <p className={styles.address}>
+          2356 K. Laquie Rd #235. NewYork, 85022 (602) 867-1011
+        </p>
+        <p className={styles.address}>
+          1614 E. Erwin St #104. NewYork, 85022 (602) 867-1012
+        </p>
+        <p className={styles.address}>
+          1614 W. Caroll St #125. NewYork, 85022 (602) 867-1013
+        </p>
+      </div>
+      <div className={styles.workContainer}>
+        <h4>Working hours</h4>
+        <p className={styles.address}>
+          MONDAY UNTIL FRIDAY
+          <br /> 9:00 – 22:00
+        </p>
+        <p className={styles.address}>
+          SATURDAY - SUNDAY
+          <br /> 12:00 – 24:00
+        </p>
+      </div>
     </div>
   );
 };
