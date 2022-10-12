409999fb98855816b4e0b91e2eabb471e7f67d27
adibrisan <adi.brisan@yahoo.com>
2022-10-12 15:23:06 +0300

    Centered list items in nav bar

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -20,27 +20,27 @@ const Footer = () => {
       <div className={styles.addressContainer}>
         <h4>Find our restaurants</h4>
         <p className={styles.address}>
-          1654 R. Don Road #304. NewYork, 85022 (602) 867-1010
+          1654 R. Don Road #304. NewYork, 85022 (602) 867&ndash;1010
         </p>
         <p className={styles.address}>
-          2356 K. Laquie Rd #235. NewYork, 85022 (602) 867-1011
+          2356 K. Laquie Rd #235. NewYork, 85022 (602) 867&ndash;1011
         </p>
         <p className={styles.address}>
-          1614 E. Erwin St #104. NewYork, 85022 (602) 867-1012
+          1614 E. Erwin St #104. NewYork, 85022 (602) 867&ndash;1012
         </p>
         <p className={styles.address}>
-          1614 W. Caroll St #125. NewYork, 85022 (602) 867-1013
+          1614 W. Caroll St #125. NewYork, 85022 (602) 867&ndash;1013
         </p>
       </div>
       <div className={styles.workContainer}>
         <h4>Working hours</h4>
         <p className={styles.address}>
           MONDAY UNTIL FRIDAY
-          <br /> 9:00 – 22:00
+          <br /> 9:00 &ndash; 22:00
         </p>
         <p className={styles.address}>
           SATURDAY - SUNDAY
-          <br /> 12:00 – 24:00
+          <br /> 12:00 &ndash; 24:00
         </p>
       </div>
     </div>
